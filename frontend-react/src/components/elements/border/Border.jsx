4ecff5b619e9a06e70ed1b04901c75d9c19f931b
Mushi <144794503+Mouche00@users.noreply.github.com>
2024-04-16 17:03:51 +0100

    add fake scroll

--- frontend-react/src/components/elements/border/Border.jsx
+++ frontend-react/src/components/elements/border/Border.jsx
@@ -2,15 +2,17 @@
 import BorderImage from 'src/assets/border.png'
 import './Border.css'
 
-const Border = ({ animated = false, direction, customImage, className }) => {
+const Border = ({ animated = false, direction, customImage, className, onClick }) => {
     return (
-        <div className={`absolute z-10 h-full w-10 top-0 ${className} ${direction == 'left' ? 'left-0 translate-x-[-50%]' : 'right-0 translate-x-[50%]'}`}>
+        <div onClick={onClick} className={`${onClick ? 'cursor-pointer' : ''} absolute z-10 h-full w-10 top-0 ${className} ${direction == 'left' ? 'left-0 translate-x-[-50%]' : 'right-0 translate-x-[50%]'}`}>
             <div
                 className={`${animated ? direction == 'left' ? 'scroll-left' : 'scroll-right' : ''} h-full w-full bg-repeat bg-contain`} style={{ backgroundImage: `url(${customImage ?? BorderImage})` }}>
             </div>
-            <div
-                className={`${animated ? direction == 'left' ? 'scroll-left' : 'scroll-right' : ''} h-full w-full bg-repeat bg-contain`} style={{ backgroundImage: `url(${customImage ?? BorderImage})` }}>
-            </div>
+            {animated && (
+                <div
+                    className={`${animated ? direction == 'left' ? 'scroll-left' : 'scroll-right' : ''} h-full w-full bg-repeat bg-contain`} style={{ backgroundImage: `url(${customImage ?? BorderImage})` }}>
+                </div>
+            )}
         </div>
     )
 }
--- frontend-react/src/components/pages/MediumPage.jsx
+++ frontend-react/src/components/pages/MediumPage.jsx
@@ -1,29 +1,36 @@
+/* eslint-disable react/display-name */
 import Loader from "../elements/loader/Loader"
 import DefaultWallpaper from 'src/assets/default-wallpaper.jpg'
 import DefaultPoster from 'src/assets/default-poster.jpg'
 import BorderRight from 'src/assets/border-5-v.png'
 import Texture from 'src/assets/texture.jpg'
 import Border from "../elements/border/Border"
-import { useRef, useState } from "react"
+import { forwardRef, useEffect, useRef, useState } from "react"
 
-const Diamond = ({ label, className, onClick }) => {
+const Section = forwardRef(({ children, onWheel, id, section }, ref) => {
+    let pos = ''
+    switch (true) {
+        case id < section:
+            pos = 'translate-y-[-100%]'
+            console.log('up')
+            break;
+        case id > section:
+            pos = 'translate-y-[100%]'
+            console.log('down')
+            break;
+    }
+    console.log('here', id, section, pos)
     return (
-        <button onClick={() => onClick(label)} className={`relative w-32 h-32 mx-auto border-4 border-white rounded rotate-[-45deg] ${className}`}>
-            <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
-                <div className="absolute w-48 h-48 bg-cover bg-center bg-no-repeat rotate-[45deg]" style={{backgroundImage: `url(src/assets/diamond-${label}.webp)`}}></div>
-            </div>
-            <div className="absolute top-0 left-0 h-full w-full flex items-end justify-center rotate-[45deg]">
-                <p className="absolute bg-white font-bold w-[10rem] px-4 text-center text-xl">{label.toUpperCase()}</p>
-            </div>
-        </button>
+        <div ref={ref} onWheel={onWheel} className={`absolute transition-all w-full h-full grid grid-cols-3 gap-8 ${pos}`}>
+            {children}
+        </div>
     )
-}
+})
 
-const MediumPage = () =>{
+const InfoSection = forwardRef(({ onWheel, id, section }, ref) => {
+
     const [activeLabel, setActiveLabel] = useState('')
     const sidebar = useRef(null)
-    const bottomSibling = useRef(null)
-    const topSibling = useRef(null)
 
     const labels = [
         'title',
@@ -39,16 +46,116 @@ const MediumPage = () =>{
 
     const handleClick = (label) => {
         if(label !== activeLabel){
-            sidebar.current.classList.toggle('translate-x-[100%]')
-            if(activeLabel !== '') {
-                setTimeout(() => {
-                    sidebar.current.classList.toggle('translate-x-[100%]')
-                }, 200)
-            }
-            
-            setActiveLabel(label)
+            sidebar.current.classList.add('translate-x-[100%]')
+            console.log(2)
+            setTimeout(() => {
+                sidebar.current.classList.remove('translate-x-[100%]')
+                setActiveLabel(label)
+            }, 200)
         }
     }
+    
+    return (
+        <Section ref={ref} onWheel={onWheel} id={id} section={section}>
+            <div className="gap-32 my-12 mx-auto h-fit w-fit col-span-2 grid grid-cols-3 p-16">
+                {labels.map((label, i) => (
+                    <Diamond onClick={handleClick} key={i} label={label} className={(i - 1) % 3 == 0 ? 'translate-y-[100%]' : ''} />
+                ))}
+            </div>
+
+            <Sidebar ref={sidebar}>
+                {activeLabel ? (
+                    <>
+                        <div className="w-full h-[50%] bg-contain bg-repeat bg-center opacity-70" style={{backgroundImage: `url(src/assets/diamond-${activeLabel}.webp)`}}></div>
+                        <h1 className="font-black text-black text-5xl bg-white w-full text-center p-2">{activeLabel.toUpperCase()}</h1>
+                        <p className="text-white p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi impedit consectetur ut reiciendis nihil perferendis sunt sapiente possimus nulla obcaecati, veniam aut quo magnam sint illo repudiandae. Nulla, sed quasi!</p>    
+                    </>
+                ) : (
+                    <div className="w-full h-full flex items-center justify-center">
+                        <h1 className="rotate-[-90deg] text-9xl text-white font-black">SELECT A SECTION</h1>
+                    </div>
+                )}
+                </Sidebar>
+        </Section>
+    )
+})
+
+const Diamond = ({ label, className, onClick }) => {
+    return (
+        <button onClick={() => onClick(label)} className={`relative w-32 h-32 mx-auto border-4 border-white rounded rotate-[-45deg] ${className}`}>
+            <div className="relative w-full h-full flex items-center justify-center overflow-hidden">
+                <div className="absolute w-48 h-48 bg-cover bg-center bg-no-repeat rotate-[45deg]" style={{backgroundImage: `url(src/assets/diamond-${label}.webp)`}}></div>
+            </div>
+            <div className="absolute top-0 left-0 h-full w-full flex items-end justify-center rotate-[45deg]">
+                <p className="absolute bg-white font-bold w-[10rem] px-4 text-center text-xl">{label.toUpperCase()}</p>
+            </div>
+        </button>
+    )
+}
+
+const Sidebar = forwardRef(({ children }, ref) => {
+    const hideSidebar = (e) => {
+        e.stopPropagation()
+        ref.current.classList.toggle('translate-x-[100%]')
+    }
+
+    return (
+        <div ref={ref} className="relative transition-all translate-x-[100%] w-full h-full top-0 left-0 bg-contain bg-repeat bg-center" style={{backgroundImage: `url(${Texture})`}}>
+            <Border onClick={hideSidebar} direction='left' className='w-8' customImage={BorderRight} />
+            <div className="h-full flex items-center flex-col">
+                {children}
+            </div>
+        </div>
+    )
+})
+
+const MediumPage = () =>{
+    const [activeSection, setActiveSection] = useState(0)
+    const sections = useRef(null)
+    // const [currentSec, setCurrentSec] = useState(null)
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setCurrentSec(topElement.current)
+    //     }, 1500)
+    // }, [])
+
+    const handleScroll = (e) => {
+        const currentSec = sections.current.children[activeSection]
+        const nextSec = currentSec.nextElementSibling
+        const prevSec = currentSec.previousElementSibling
+        console.log('curr', currentSec)
+        console.log('next', nextSec)
+        console.log('prev', prevSec)
+        let pos = 0
+        if(e.deltaX <= 0) {
+            if(e.deltaY > 0 && nextSec){
+                pos++
+                // currentSec.classList.add('translate-y-[-100%]')
+                // nextSec.classList.add('translate-y-[-100%]')
+            } else if(e.deltaY < 0 && prevSec){
+                pos--
+                // currentSec.classList.remove('translate-y-[-100%]')
+                // prevSec.classList.remove('translate-y-[-100%]')
+
+            }
+        }
+
+        switch (true) {
+            case (pos > 0):
+                setTimeout(() => {
+                    setActiveSection(activeSection + 1)
+                }, 100)
+                break;
+            case (pos < 0):
+                setTimeout(() => {
+                    setActiveSection(activeSection - 1)
+                }, 100)
+                break;
+        }
+    }
+
+    console.log(activeSection)
 
     const handleUpScroll = (e) => {
         if(e.deltaY > 0 && e.deltaX <= 0){
@@ -76,25 +183,10 @@ const MediumPage = () =>{
                 </div>
 
                 <div className="bg-cover bg-no-repeat bg-center w-full h-full" style={{backgroundImage: `url(${DefaultWallpaper})`}}>
-                    <div className="bg-bronze bg-opacity-60 h-full w-full">
-                        <div ref={topSibling} onWheel={handleUpScroll} className="transition-all w-full h-full grid grid-cols-3 gap-8">
-                            <div className="gap-32 my-12 mx-auto h-fit w-fit col-span-2 grid grid-cols-3 p-16">
-                                {labels.map((label, i) => (
-                                    <Diamond onClick={handleClick} key={i} label={label} className={(i - 1) % 3 == 0 ? 'translate-y-[100%]' : ''} />
-                                ))}
-                            </div>
-                            <div ref={sidebar} className="relative transition-all translate-x-[100%] w-full h-full top-0 left-0 bg-contain bg-repeat bg-center" style={{backgroundImage: `url(${Texture})`}}>
-                                <Border direction='left' className='w-8' customImage={BorderRight} />
-                                <div className="h-full flex items-center flex-col">
-                                    <div className="w-full h-[50%] bg-contain bg-repeat bg-center opacity-70" style={{backgroundImage: `url(src/assets/diamond-${activeLabel}.webp)`}}></div>
-                                    <h1 className="font-black text-black text-5xl bg-white w-full text-center p-2">{activeLabel.toUpperCase()}</h1>
-                                    <p className="text-white p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi impedit consectetur ut reiciendis nihil perferendis sunt sapiente possimus nulla obcaecati, veniam aut quo magnam sint illo repudiandae. Nulla, sed quasi!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div ref={bottomSibling} onWheel={handleDownScroll} className="transition-all w-full h-full grid grid-cols-3 gap-8">
-                            <p className="text-9xl text-white">hello</p>
-                        </div>
+                    <div ref={sections} className="relative bg-bronze bg-opacity-60 h-full w-full">
+                        <InfoSection id={0} section={activeSection} onWheel={handleScroll} />
+                        <InfoSection id={1} section={activeSection} onWheel={handleScroll} />
+                        <InfoSection id={2} section={activeSection} onWheel={handleScroll} />
                     </div>
                 </div>
             </div>
